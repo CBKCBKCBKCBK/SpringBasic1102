0a31c870deb8708f3e5aa29c577d5a194651254d
Bohong.Chen <cbkcbkcbkcbk@gmail.com>
2020-11-09 18:41:33 +0800

    上傳

--- src/main/java/com/mycompany/springbasic1102/di/Student.java
+++ src/main/java/com/mycompany/springbasic1102/di/Student.java
@@ -2,13 +2,23 @@ package com.mycompany.springbasic1102.di;
 
 public class Student {  //pojo
     private String name;
-    private int age;
-
-    @Override
-    public String toString() {
-        return "Student{" + "name=" + name + ", age=" + age + '}';
+    private Integer age;
+    private Teacher teacher;
+    
+    public void init(){
+        System.out.println("init");
+    }
+    public void destroy(){
+        System.out.println("destroy");
     }
 
+    public Student() {
+    }
+
+    public Student(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
     public String getName() {
         return name;
     }
@@ -17,11 +27,26 @@ public class Student {  //pojo
         this.name = name;
     }
 
-    public int getAge() {
+    public Integer getAge() {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name 
+                + ", age=" + age 
+                + ", teacher=" + (teacher==null?"":teacher.getName()) + '}';
     }
 }
